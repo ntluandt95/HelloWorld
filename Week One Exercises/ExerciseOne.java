a492e76c39f951911e935557eaa16b376d1b4d91
ntluan95 <ntluandt95@gmail.com>
2021-09-10 17:33:20 -0500

    week one exercises

---
+++ Week One Exercises/ExerciseOne.java
@@ -0,0 +1,8 @@
+public class ExerciseOne {
+	public static void main(String[] args) {
+		double first = 10.5;
+	    double second = 11.5;
+	    double multi = first*second;
+	    System.out.print(multi);
+	}
+}
